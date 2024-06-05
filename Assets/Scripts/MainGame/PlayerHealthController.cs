5d3b19681a3d4ed0656010cb74cb914632dbc794
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-06-05 18:33:57 +0300

    Player Health Base

---
+++ Assets/Scripts/MainGame/PlayerHealthController.cs
@@ -0,0 +1,66 @@
+using Fusion;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthController : NetworkBehaviour
+{
+    [SerializeField] private Image fillAmountImg;
+    [SerializeField] private TextMeshProUGUI healthAmountText;
+
+
+    [Networked(OnChanged = nameof(HealthAmountChanged))] private int currentHealthAmount { get; set; }
+
+    private const int MAX_HEALTH_AMOUNT = 100;
+
+    public override void Spawned()
+    {
+        currentHealthAmount = MAX_HEALTH_AMOUNT;
+    }
+
+    private static void HealthAmountChanged(Changed<PlayerHealthController> changed)
+    {
+        var currentHealth = changed.Behaviour.currentHealthAmount;
+
+        changed.LoadOld();
+        var oldhealthAmount = changed.Behaviour.currentHealthAmount;
+
+        //only if the current health is not the same as the prev one
+        if (currentHealth != oldhealthAmount) 
+        {
+            changed.Behaviour.UpdateVisuals(currentHealth);
+
+            //we did not respawn of just spawned
+            if (currentHealth != MAX_HEALTH_AMOUNT) 
+            {
+                changed.Behaviour.PlayerGotHit(currentHealth);
+            }
+        }
+    }
+
+    private void UpdateVisuals(int healthAmount) 
+    {
+        var num = (float)healthAmount / MAX_HEALTH_AMOUNT;
+        fillAmountImg.fillAmount = num;
+        healthAmountText.text = $"{healthAmount}/{MAX_HEALTH_AMOUNT}";
+    }
+
+    private void PlayerGotHit(int healthAmount) 
+    {
+        var isLocalPlayer = Runner.LocalPlayer == Object.HasInputAuthority;
+        if (isLocalPlayer) 
+        {
+            //todo do hit effects
+            Debug.Log("LOCAL PLAYER GOT HIT!");
+        }
+
+        if (healthAmount <= 0) 
+        {
+            //todo kill and respawn the player
+            Debug.Log("Player is DEAD!");
+        }
+    }
+}
