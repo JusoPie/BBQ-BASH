e28d180670ddc01236c0b13864f606684c5dc0a4
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-07-17 19:14:57 +0300

    meatman (WIP)
    meatman does not have a bubbleshield yet

---
+++ Assets/Scripts/MainGame/Enemies/ShieldedEnemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Fusion;
+
+public class ShieldedEnemy : EnemyController
+{
+    private Animator animator;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+
+    public override void TakeDamage(int damage, int attackerId)
+    {
+        base.TakeDamage(damage, attackerId);
+        RPC_PlayDamageAnim();
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_PlayDamageAnim()
+    {
+        animator.SetTrigger("DamageMeatman");
+    }
+}
